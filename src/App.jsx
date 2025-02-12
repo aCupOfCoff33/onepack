f80eda5cca9cb365509356038b128932389574f9
Aaryan Joharapurkar <aaryanj@outlook.com>
2025-02-12 12:56:31 -0500

    added vercel tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import HomePage from "./HomePage";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <div>
         <HomePage />
+        <Analytics />
       </div>
     </>
   );
